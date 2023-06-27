0228c8e8734be2766752941d04ac89ff166e7cf1
Suma-Geethika-Brahmakanti <129078671+Suma-Geethika-Brahmakanti@users.noreply.github.com>
2023-06-27 12:37:37 +0530

    First Commit

---
+++ enroll.js
@@ -0,0 +1,4 @@
+function enroll(){
+    document.getElementById("view").innerHTML="Congragulations! You have enrolled to this course <br> Happy Learning!!";
+    document.getElementsByClassName("enroll")[0].innerHTML="Enrolled";
+}
